1430592bb1b2e9b708f80178bc3d53d31f70176f
deathault <deathault@yandex.ru>
2025-01-21 23:31:26 +0300

    Add zoom

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,15 @@
 import Link from 'next/link';
-
+import Image from 'next/image';
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
+
+
+
+      <Link
+          href="/docs"
+          className="mx-auto mb-12 relative max-w-xs overflow-hidden bg-cover bg-no-repeat"
+        ><Image src="arch_logo.png" alt='Логотип Arch Linux' className="max-w-xs transition duration-300 ease-in-out hover:rotate-90" width={156} height={156}></Image></Link>
       <h1 className="mb-4 text-2xl font-bold">Гайд Arch Linux</h1>
       <p className="text-fd-muted-foreground">
         Привет друг, это гайд по моему любимому дистрибутиву. Открывай {' '}
@@ -13,7 +20,9 @@ export default function HomePage() {
           /docs
         </Link>{' '}
         для просмотра.
-      </p>
+      </p><div className="mx-auto mt-3">
+      <Link href="https://t.me/deathault" target='_blank'><Image src="deathault.jpg" alt='deathault telegram' width={55} height={55} className='mx-auto mt-12 rounded-full ring-2 ring-gray-700 hover:ring-white'></Image></Link>
+    </div>
     </main>
   );
 }
